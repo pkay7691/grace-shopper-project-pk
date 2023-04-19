be3f72d9808b3a943d1adf577b8ea3d1653a2026
Patrick Kenny <patrickmkenny91@gmail.com>
2023-04-19 14:47:30 -0500

    revamped home page

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -7,9 +7,14 @@ import { useSelector } from "react-redux";
 const Home = (props) => {
   return (
     <div className="welcome">
-      <div className="message">
+      <div className="welcome-cat-shopper">
         <h3>Welcome to CatShopper!</h3>
       </div>
+      <div className="message">
+        <h3>"Buying cats has never been this easy" - Local
+           Cat Lady</h3>
+      </div>
+      {/* <img id='welcome-cat-shopper' src="bigcatshopper-removebg-preview.png" /> */}
     </div>
   );
 };
