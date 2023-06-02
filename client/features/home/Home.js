8d54eaa71ccb796eb34b77e021d3f549ad7824c5
Patrick Kenny <patrickmkenny91@gmail.com>
2023-06-02 16:10:53 -0500

    explore button functionality

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 /**
  * COMPONENT
  */
 const Home = (props) => {
+  const navigate = useNavigate();
+
   return (
     <div className="welcome">
-
-      <video id='cat-video' autoPlay muted loop>
-        <source src='cat_in_the_sun.mp4' type='video/mp4' />
-      </video> 
-      <div className='welcome-message'>Find Your Forever Friend</div>
-      <button className='welcome-button'>Explore</button>
+      <video id="cat-video" autoPlay muted loop>
+        <source src="cat_in_the_sun.mp4" type="video/mp4" />
+      </video>
+      <div className="welcome-message">Find Your Forever Friend</div>
+        <button onClick={(e) => navigate('/products')} className="welcome-button">Explore</button>
+   
     </div>
   );
 };
